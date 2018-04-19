dffd2d06f6454df240e39863c2c45643ed63f56c
teja <teja.nspe@gmail.com>
2018-04-18 20:21:40 -0700

    First Check-in

--- teja-angular/src/app/app.module.ts
+++ teja-angular/src/app/app.module.ts
@@ -1,16 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-
 import { AppComponent } from './app.component';
-
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+import { SuccessAlertComponent } from './success-alert/success-alert.component';
+import { WarningAlertComponent } from './warning-alert/warning-alert.component';
 
 @NgModule({
   declarations: [
-    AppComponent
-  ],
+    AppComponent,
+    SuccessAlertComponent,
+    WarningAlertComponent,
+     ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpModule
   ],
   providers: [],
   bootstrap: [AppComponent]
